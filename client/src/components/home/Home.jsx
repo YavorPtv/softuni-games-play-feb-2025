ef923d314ee054faa3de26c1eb3b7613c25699aa
YavorPtv <yavorpetrakiev@gmail.com>
2025-03-20 22:02:07 +0200

    Add key prop

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -3,7 +3,6 @@ import { useLatestGames } from "../../api/gameApi";
 export default function Home() {
     const { latestGames } = useLatestGames();
 
-    console.log(latestGames);
     return (
         <section id="welcome-world">
 
@@ -17,7 +16,7 @@ export default function Home() {
                 <h1>Latest Games</h1>
 
                 {latestGames.map(game => (
-                    <div className="game">
+                    <div className="game" key={game._id}>
                         <div className="image-wrap">
                             <img src={game.imageUrl} />
                         </div>
